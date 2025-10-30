3e1dfedd4a7c86a9376910feabb7ff134da1c31f
zeysdzl <zeynepsduzel@gmail.com>
2025-10-30 18:02:53 +0300

    Delete all comment blocks

--- app/src/main/java/com/zsd/eticaretuygulamasi/ui/screens/profil_sayfa/adres/YeniAdresSayfa.kt
+++ app/src/main/java/com/zsd/eticaretuygulamasi/ui/screens/profil_sayfa/adres/YeniAdresSayfa.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
